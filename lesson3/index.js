d79c6e29ecc12f358178650f88de1850348272f2
Pagud <pogudin2002@yandex.ru>
2022-02-26 12:49:24 +0300

    2.5 урока по JS )))

---
+++ lesson3/index.js
@@ -0,0 +1,98 @@
+// //примитивные типы данных
+
+// // nuimber
+// //console.log(1 / 0);
+
+// console.log(typeof NaN); 
+// console.log(1)
+
+// //оператор typeof показывает тип данных, указанный typeof
+// //string
+// const myString = "строка";
+// const anotherString = "строка";
+// console.log(myString);
+
+// const templateString = `шаблонная строка ${anotherString}`;
+// console.log(templateString); 
+
+// //Boolean
+
+// const isAdmin = "true";
+// const inUser  = "fals":
+
+// //null
+// const emptyVariable = null;
+
+// //undefaind
+// const userName ;
+// console.log(userName);
+
+
+// не примитивные типы данных
+// const user = {
+//     name: "vasya",
+//     lastName: "Ivanov",
+//     age: 23,
+//     citizenship: "Russia",
+//     newObject:{
+//         newProperty: "property",
+//     }
+// };
+// const userName = "user.name";
+
+// оператор typeof
+
+//console.log(typeof null);
+
+
+//преобразование типов данных
+//Строковое преобразование
+// const myVar = true
+// const myVar = 123
+// const meVarToString = String(myVar);
+
+// const myVar = "aoe123";
+// const myVarToNumber = +myVar;
+// console.log(typeof myVarToNumber);
+
+
+//логическое преобразование
+
+// const myVar = "0";
+// console.log(!!myVar);
+
+//Матаематичесикие опраторы
+//Бинарный плюс
+
+// const mYNumber = 1 ;
+// const newNumber = 3 ;
+// const myString = "строка";
+
+// console.log(mYNumber + myString);
+
+//сравнение строк
+
+// const string1 = "сТрока";
+// const string2 = "солнце";
+
+// console.log(string1 > string2);
+
+//unicode
+
+// const userTypes = {
+//     admin: "admin",
+//     user: "user",
+//     teacher: 'teacher',
+//     contentManager: 'contentManager'
+// };
+
+// const serverDataUserType = "user";
+
+// if (serverDataUserType === userTypes.admin) {
+//     console.log('показать весь контент');
+// } else {
+//     console.log("показать ограниченный контент");
+// }
+
+
+console.log(12 / 3);
