6f38212f032228950a344c13823c1628d8d4c055
Pagud <pogudin2002@yandex.ru>
2022-03-28 11:38:08 +0300

    Домашка от Павла

---
+++ JScoach/index.js
@@ -0,0 +1,82 @@
+// const a = 1; 
+// if (a === 0) {
+//     console.log("Верно")
+// } else {
+//     console.log("Неверно")
+// };
+
+// a === 0 ? console.log("Верно"): console.log("Неверно");
+
+// ДОМАШНЯЯ ПРАКТИКА
+
+// 1 Вывести в терминал строку “Hello World!”
+
+// console.log("Hello World!");
+
+// 2 Создать переменную для каждого типа данных
+
+// const age = 10; // числа
+// console.log(age);
+
+// let name = "Ivan"; // строки
+// console.log(name);
+
+// let isUser = false; // boolean
+// let isNotUser = true; //boolean
+
+// let nameUser = null; // null
+// console.log(nameUser)
+
+// let userName; // undefiend
+// console.log(userName);
+
+// object
+// let user = {
+//     name: "Ivan",
+//     age: "18",
+// };
+// console.log(user);
+
+// тип simbol используется для создания уникальных переменных то же что и строка или число
+
+//тип bigint может большие цыфры больше и меньше 52 степени
+
+// 3 Вывести ее тип через оператор typeof и функцию typeof
+// console.log(typeof 42); //NaN
+// console.log(typeof 'Ivan');
+// console.log(typeof true);
+// console.log(typeof null); //объект
+// console.log(typeof asfddsgs); //undefiend
+
+// Попробовать изменить переменную объявленную через const
+// const age = 10;
+// let age = 11
+
+// Попробовать изменить переменную с типом object объявленную через const
+
+// const user = {
+//     name: "Dima",
+//     age: 33,
+// };
+// user.name = "Olesya";
+// console.log(user.name);
+
+// Попробовать изменить переменную с типом object объявленную через let
+
+// let user = {
+//     name: "Dima",
+//     age: 33
+// };
+// user = "Olesya";
+// console.log(user);
+
+var user = {
+    name: "Dima",
+    age: 33,
+};
+user = "Olesya";
+console.log(user);
+
+
+
+
